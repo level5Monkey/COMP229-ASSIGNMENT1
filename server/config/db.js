20744ba4391dc94ef52c03407a06de3c9d5df424
Jiaxing Li <henrylovewatermelon@gmail.com>
2020-10-27 01:34:42 +0800

    fix mongodb

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ module.exports =
     //"URI": "mongodb+srv://user:123@mymongodb.iyfbd.mongodb.net/myMongoDB?retryWrites=true&w=majority",
     //"URI": "mongodb+srv://jiaxing:123@mymongodb.iyfbd.mongodb.net/myMongoDB?retryWrites=true&w=majority",
     //"URI": "mongodb://localhost/mean",
-    "URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    //"URI": "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority",
+    "URI": "mongodb+srv://jiaxing:ym9EI2W895W3GrID@1027.ja9zx.mongodb.net/mean?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
 }
